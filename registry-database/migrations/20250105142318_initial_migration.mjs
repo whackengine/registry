4907264623e678f36e6be2d77297db5698c509f0
hydroperfox <hydroperfox@gmail.com>
2025-01-05 12:45:34 -0300

    Update

--- registry-database/migrations/20250105142318_initial_migration.mjs
+++ registry-database/migrations/20250105142318_initial_migration.mjs
@@ -4,7 +4,51 @@
  * @returns { Promise<void> }
  */
 export const up = async (knex) => {
-  
+  return knex.schema
+    .createTable("users", table => {
+      table.bigIncrements("id");
+      table.datetime("registered_at").defaultTo(knex.fn.now());
+      table.boolean("admin").defaultTo(false);
+      table.string("fullname", 127).notNullable();
+      table.string("github_access_token", 511).notNullable();
+    })
+    .createTable("maintenance_invites", table => {
+      table.bigIncrements("id");
+      table.bigint("package_id");
+      table.bigint("invitee_user_id");
+      table.bigint("invited_by_user_id");
+    })
+    .createTable("packages", table => {
+      table.bigIncrements("id");
+      table.string("name", 127).notNullable();
+      table.string("description", 512).notNullable();
+      table.string("keywords", 512).notNullable();
+      table.string("categories", 512).notNullable();
+    })
+    .createTable("maintainers", table => {
+      table.bigIncrements("id");
+      table.bigint("user_id");
+      table.bigint("package_id");
+    })
+    .createTable("package_versions", table => {
+      table.bigIncrements("id");
+      table.string("version_number", 32).notNullable();
+      table.datetime("published_at").defaultTo(knex.fn.now());
+      table.string("readme", 50000).notNullable();
+      table.binary("zip");
+      table.bigint("package_id");
+      table.boolean("yanked");
+      table.bigint("downloads").defaultTo(0);
+      table.datetime("downloads_this_week_at").defaultTo(knex.fn.now());
+      table.bigint("downloads_this_week").defaultTo(0);
+    })
+    .createTable("dependencies", table => {
+      table.bigIncrements("id");
+      table.bigint("dependency_package_id");
+      table.string("dependency_version_number", 32).notNullable();
+      table.bigint("dependent_package_id");
+      table.string("dependent_version_number", 32).notNullable();
+    });
 };
 
 /**
