ef4bcfd17b78b605fd878264e2aa6e66b80c945d
hydroperfox <hydroperfox@gmail.com>
2025-01-05 11:23:29 -0300

    Update

---
+++ registry-database/knexfile.mjs
@@ -0,0 +1,57 @@
+import "dotenv/config";
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+
+  development: {
+    client: "mysql",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: "knex_migrations"
+    },
+  },
+
+  staging: {
+    client: "mysql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    },
+  },
+
+  production: {
+    client: "mysql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  migrations: {
+    client: "mysql",
+    connection: process.env.DATABASE_URL,
+
+    directory: "./migrations",
+    loadExtensions: [".mjs"],
+  },
+
+  seeds: {
+    client: "mysql",
+    connection: process.env.DATABASE_URL,
+
+    directory: "./seeds",
+    loadExtensions: [".mjs"],
+  },
+};
--- registry-database/src/index.js
+++ registry-database/src/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import knexCreator from "knex";
 
-const knex = knexCreator({
+export const knex = knexCreator({
     client: "mysql",
     connection: process.env.DATABASE_URL,
     searchPath: ["knex", "public"],
